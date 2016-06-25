3c41125ced9d329c68245ca72cdd37b732f562e9
James Edward Lewis II <lewisje@users.noreply.github.com>
2016-06-25 15:33:24 -0400

    better Google form, don't erase existing onerror

--- browser.js
+++ browser.js
@@ -1,6 +1,13 @@
-(function () {
-    window.onerror = function (message, file, line, col, error) {
-        var win = window.open('https://www.google.com/#q=' + encodeURIComponent(message), '_blank');
+(function (w) {
+    'use strict';
+    var onerror = w.onerror;
+    function dumbugger(message, file, line, col, error) {
+        var win = w.open('https://encrypted.google.com/search?pws=0&safe=off&num=100&complete=0&q=' + encodeURIComponent(message), '_blank');
+        win.opener = null;
         win.focus();
-    };
-})();
+    }
+    w.onerror = typeof onerror = 'function' ? function (message, file, line, col, error) {
+        onerror(message, file, line, col, error);
+        dumbugger(message, file, line, col, error);
+    } : dumbugger;
+})(window);
